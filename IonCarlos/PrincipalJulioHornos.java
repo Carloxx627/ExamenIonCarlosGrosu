cdc68ec23dde7bd6fbcc2dde7edf529312839a9b
Ion Carlos Grosu <bichosexualcr7.siu@gmail.com>
2025-06-13 15:41:29 +0200

    Commit inicial

---
+++ IonCarlos/PrincipalJulioHornos.java
@@ -0,0 +1,14 @@
+package IonCarlos;
+
+public class PrincipalJulioHornos {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		ImprimirJulioHornos miImprimirJulioHornos = new ImprimirJulioHornos();
+		
+		miImprimirJulioHornos.imprimirDatosJulioHornos("Julio", "Hornos", 1973, "Soria");
+		
+		
+	}
+
+}
